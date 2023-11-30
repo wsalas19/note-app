3d058710dc7d99538ae680bf8ffb8f40f4a86534
wsalas19 <willbullet52@gmail.com>
2023-11-30 11:46:17 -0500

    fix url typo

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.use(
 		cors({
-			origin: ["http://localhost:5173", "note-app-zeta-ten.vercel.app"],
+			origin: ["http://localhost:5173", "https://note-app-zeta-ten.vercel.app/"],
 		}),
 	);
 	await app.listen(3000);
